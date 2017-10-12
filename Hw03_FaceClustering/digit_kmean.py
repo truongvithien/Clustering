9653feecc1760800ee42d99a4a1b4c701b27c6f1
Truong Vi Thien <thientruong@thientv.localdomain>
2017-10-12 11:53:21 +0700

    Upload default KMean and Spectral for Digit data

---
+++ Hw03_FaceClustering/digit_kmean.py
@@ -0,0 +1,52 @@
+# Learner: Truong Vi Thien (14520874)
+# Digit data clustering using K-Mean.
+
+import numpy as np
+from time import time
+
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use("ggplot")
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+# Load data
+
+digits = load_digits()
+data = scale(digits.data)
+
+n_samples, n_features = data.shape
+n_digits = len(np.unique(digits.target))
+labels = digits.target
+
+sample_size = 300
+
+print("Input: \n n_digits = n_cluster: %d, \t n_samples: %d, \t n_features: %d"
+      % (n_digits, n_samples, n_features))
+
+print(82 * '_')
+
+# Principal components analysis aka Reduce data into 2 dimensions
+reduced_data = PCA(n_components=2).fit_transform(data)
+
+
+# Using KMean
+kmeans = KMeans(n_clusters=n_digits)
+kmeans.fit(reduced_data)
+
+centroids = kmeans.cluster_centers_
+labels = kmeans.labels_
+
+# Visualize
+print(centroids)
+print(labels)
+
+plt.title('Digit data clustering using K-Mean')
+plt.scatter(reduced_data[:, 0], reduced_data[:, 1], c=labels, marker='.', s=69, linewidths=3, zorder=10)
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='o', s=69, linewidths=5, color='w', edgecolors="k", alpha=0.5, zorder=10)
+plt.show()
