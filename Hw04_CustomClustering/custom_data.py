8b32b51fca7be06740b9124ee22f694a1b960d71
Truong Vi Thien <thientruong@thientv.localdomain>
2017-10-20 10:31:13 +0700

    Clone HW03

---
+++ Hw04_CustomClustering/custom_data.py
@@ -0,0 +1,32 @@
+# Learner: Truong Vi Thien (14520874)
+# Pet dataset (~200 images per class - 37 class)
+# http://www.robots.ox.ac.uk/~vgg/data/pets/
+
+import numpy as np
+
+from sklearn.datasets import fetch_lfw_people
+from skimage.feature import local_binary_pattern
+
+# Load data
+
+faces = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
+
+
+data = np.array([]).reshape(0,1850)
+for image in faces.images:
+    feature = local_binary_pattern(image, P=8, R=0.5).flatten()
+    data = np.append(data,[feature],axis=0)
+
+targets = faces.target
+target_names = faces.target_names
+n_classes = target_names.shape
+
+np.save(file='data_target.npy', arr=targets)
+np.save(file='data_target_name.npy', arr=target_names)
+np.save(file='data_feature.npy', arr=data)
+
+
+# Check
+print("Datasets loaded! ")
+print("n classes: %d" %n_classes)
+print("n samples: %d" %len(faces.images))
